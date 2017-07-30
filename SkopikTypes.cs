dc44f6009a6600ce651199d7db1e9b33c23f0045
Mark Ludwig <mk.ludwig1@gmail.com>
2017-07-29 17:01:07 -0700

    Updated types and added some documentation in a few places.
    * Added `ToString()` override to `SkopTypeObject<T>` class
    * Added `this[int]` accessor to `SkopikArray` for accessing objects
      via their index
    * Added `GetObjectIndex(ISkopikObject)` method to `SkopikArray`
    * Added null checks to methods that take an `ISkopikObject` argument
    * Added extension method for `ISkopikObject` in new utils class
      - `GetObjectType()` returns the `SkopikDataType` of the object
        by retreving the `SkopikTypeAttribute` assigned to the object.
    * Set `SkopikTypeAttribute` usage property 'AllowMultiple' to false
      for explicit verbosity

--- SkopikTypes.cs
+++ SkopikTypes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -111,7 +112,7 @@ namespace Skopik
         Hexadecimal,
     }
 
-    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public sealed class SkopikTypeAttribute : Attribute
     {
         /// <summary>
@@ -127,12 +128,19 @@ namespace Skopik
 
     public interface ISkopikObject
     {
+        /// <summary>
+        /// Gets the current value of the object.
+        /// </summary>
+        /// <returns>The current value of the object.</returns>
         object GetValue();
     }
-
+    
     public interface ISkopikTypeObject<T> : ISkopikObject
     {
-        T Value { get; set; }
+        /// <summary>
+        /// Gets or sets the current value of the object.
+        /// </summary>
+        T Value { get; set; }   
     }
 
     public interface ISkopikScopedObject : ISkopikObject
@@ -161,14 +169,48 @@ namespace Skopik
         T InnerData { get; }
     }
 
+    public interface ISkopikNumberObject : ISkopikObject
+    {
+        SkopikNumberType NumberType { get; set; }
+    }
+
+    public static class SkopikObjectUtils
+    {
+        /// <summary>
+        /// Gets the underlying <see cref="SkopikDataType"/> of the object.
+        /// </summary>
+        /// <param name="obj">The <see cref="ISkopikObject"/> to retrieve the type of.</param>
+        /// <returns>A <see cref="SkopikDataType"/> describing the <see cref="ISkopikObject"/>.</returns>
+        public static SkopikDataType GetObjectType(this ISkopikObject obj)
+        {
+            var type = obj.GetType();
+            var attrs = type.GetCustomAttributes(typeof(SkopikTypeAttribute), false);
+
+            if (attrs.Length == 0)
+                return SkopikDataType.None;
+
+            var attr = attrs[0] as SkopikTypeAttribute;
+
+            if (attr == null)
+                throw new InvalidOperationException("A fatal error occurred while trying to retrieve the data type of an object.");
+
+            return attr.DataType;
+        }
+    }
+
     public abstract class SkopikTypeObject<T> : ISkopikTypeObject<T>
     {
         object ISkopikObject.GetValue()
         {
             return Value;
         }
-
+        
         public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
 
         public SkopikTypeObject()
         {
@@ -180,12 +222,7 @@ namespace Skopik
             Value = value;
         }
     }
-
-    public interface ISkopikNumberObject : ISkopikObject
-    {
-        SkopikNumberType NumberType { get; set; }
-    }
-        
+    
     public abstract class SkopikNumberObject<T> : SkopikTypeObject<T>, ISkopikNumberObject
     {
         public SkopikNumberType NumberType { get; set; }
@@ -202,7 +239,7 @@ namespace Skopik
         {
             throw new InvalidOperationException("Cannot retrieve the value of a scoped object from the base type.");
         }
-
+        
         public string Name { get; set; }
 
         public bool IsAnonymous
@@ -242,12 +279,23 @@ namespace Skopik
             get { return (InnerData.Count == 0); }
         }
 
+        /// <summary>
+        /// Determines whether or not the specified <see cref="ISkopikObject"/> is present within the scope.
+        /// </summary>
+        /// <param name="obj">The <see cref="ISkopikObject"/> to find.</param>
+        /// <returns>True if the <see cref="ISkopikObject"/> was found; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="obj"/> parameter is null.</exception>
         public abstract bool IsObjectInScope(ISkopikObject obj);
     }
 
     [SkopikType(SkopikDataType.Scope)]
     public class SkopikScope : SkopikScopedTypeObject<ScopeDataValue>
     {
+        /// <summary>
+        /// Gets a <see cref="ISkopikObject"/> with the specified name in the current scope.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ISkopikObject"/> to retrieve.</param>
+        /// <returns>The <see cref="ISkopikObject"/> with the specified <paramref name="name"/>; otherwise, null.</returns>
         public ISkopikObject this[string name]
         {
             get
@@ -259,6 +307,11 @@ namespace Skopik
             }
         }
 
+        /// <summary>
+        /// Determines whether or not the <see cref="ISkopikObject"/> with the specified name is present within the scope.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ISkopikObject"/> to find.</param>
+        /// <returns>True if the <see cref="ISkopikObject"/> was found; otherwise, false.</returns>
         public bool IsObjectInScope(string name)
         {
             if (InnerData.Count > 0)
@@ -269,6 +322,9 @@ namespace Skopik
 
         public override bool IsObjectInScope(ISkopikObject obj)
         {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj), "Object cannot be null.");
+
             if (InnerData.Count > 0)
                 return InnerData.ContainsValue(obj);
 
@@ -284,15 +340,51 @@ namespace Skopik
     [SkopikType(SkopikDataType.Array)]
     public class SkopikArray : SkopikScopedTypeObject<ArrayDataValue>
     {
-        public override bool IsObjectInScope(ISkopikObject obj)
+        /// <summary>
+        /// Gets a <see cref="ISkopikObject"/> from the specified index in the scope.
+        /// </summary>
+        /// <param name="index">The index of the <see cref="ISkopikObject"/> to retrieve.</param>
+        /// <returns>The <see cref="ISkopikObject"/> with the specified <paramref name="index"/>; otherwise, null.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the <paramref name="index"/> exceeds the number of objects in the array.</exception>
+        public ISkopikObject this[int index]
         {
+            get
+            {
+                if (index >= InnerData.Count)
+                    throw new ArgumentOutOfRangeException(nameof(index), index, "Not enough objects present.");
+
+                return InnerData[index];
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the index of the specified <see cref="ISkopikObject"/> in the scope.
+        /// </summary>
+        /// <param name="obj">The object to retrieve the index of.</param>
+        /// <returns>The zero-based index of the <see cref="ISkopikObject"/> in the array. Returns -1 if not found.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="obj"/> parameter is null.</exception>
+        public int GetObjectIndex(ISkopikObject obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj), "Object cannot be null.");
+
             for (int i = 0; i < InnerData.Count; i++)
             {
                 // check against the actual reference
                 if (Object.ReferenceEquals(obj, InnerData[i]))
-                    return true;
+                    return i;
             }
-            return false;
+
+            // not found
+            return -1;
+        }
+
+        public override bool IsObjectInScope(ISkopikObject obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj), "Object cannot be null.");
+
+            return (GetObjectIndex(obj) != -1);
         }
 
         public SkopikArray(string name)
@@ -308,7 +400,7 @@ namespace Skopik
         {
             return null;
         }
-
+        
         public SkopikNull()
         {
         }
@@ -424,7 +516,7 @@ namespace Skopik
             }
             else
             {
-                throw new InvalidOperationException("Object was not found in the scope.");
+                throw new InvalidOperationException("Object is outside of the scope.");
             }
         }
 
