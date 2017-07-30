057275f13825c6ed9fa230bfc16567ee190635e7
Mark Ludwig <mk.ludwig1@gmail.com>
2017-07-29 17:04:03 -0700

    Updated SkopikData constructors, added some new overloads.
    * Added static method `Load(string filename)` that loads a file
      into memory.

--- SkopikFile.cs
+++ SkopikFile.cs
@@ -9,19 +9,46 @@ namespace Skopik
 {
     public class SkopikData
     {
+        public static readonly string DefaultName = "<global>";
+
         public SkopikScope GlobalScope { get; set; }
-        
-        public SkopikData()
+
+        public static SkopikData Load(string filename)
         {
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Skopik file was not found.");
+
+            var name = Path.GetFileNameWithoutExtension(filename);
+            var buffer = File.ReadAllBytes(filename);
+
+            return new SkopikData(name, buffer);
         }
 
+        public SkopikData()
+            : this(DefaultName) { }
+
         public SkopikData(byte[] buffer)
+            : this(DefaultName, buffer) { }
+
+        public SkopikData(string name)
+        {
+            GlobalScope = new SkopikScope(name);
+        }
+        
+        public SkopikData(string name, byte[] buffer)
         {
             using (var ms = new MemoryStream(buffer, 0, buffer.Length, false))
             using (var skop = new SkopikReader(ms))
             {
-                GlobalScope = skop.ReadScope();
-                GlobalScope.Name = "<global>";
+                GlobalScope = skop.ReadScope(name);
+            }
+        }
+
+        public SkopikData(string name, Stream stream)
+        {
+            using (var skop = new SkopikReader(stream))
+            {
+                GlobalScope = skop.ReadScope(name);
             }
         }
     }
